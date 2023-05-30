d00d07d5a022237c6a71ce2df9ab942cd9e35e7b
Hoang Quang Hung <79563371+hunine@users.noreply.github.com>
2023-05-30 14:54:04 +0700

    SMF-40: Seed data quantification (#18)

--- src/modules/quantification/entities/quantification.entity.ts
+++ src/modules/quantification/entities/quantification.entity.ts
@@ -1,15 +1,19 @@
 import { Ingredient } from 'src/modules/ingredient/entities';
 import { Recipe } from 'src/modules/recipe/entities';
 import { BaseEntity } from '@base/base.entity';
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Quantification extends BaseEntity {
-  @PrimaryColumn({ type: 'uuid', name: 'recipe_id' })
-  recipeId: string;
-
-  @PrimaryColumn({ type: 'uuid', name: 'ingredient_id' })
-  ingredientId: string;
+  @PrimaryGeneratedColumn('uuid')
+  public id!: string;
 
   @Column({ type: 'decimal' })
   public value: number;
