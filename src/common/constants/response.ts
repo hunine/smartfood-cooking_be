057220b83d7d42759265c3abf803e586d9a58469
Hoang Quang Hung <79563371+hunine@users.noreply.github.com>
2023-06-28 08:45:36 +0700

    SMF-64: Add api count recipes ingredients users (#34)

--- src/common/constants/response.ts
+++ src/common/constants/response.ts
@@ -8,6 +8,8 @@ export const RESPONSE_MESSAGES = Object.freeze({
     ERROR: 'Register failed',
   },
   USER: {
+    GET_USER_SUCCESS: 'Get user successful',
+    GET_USER_ERROR: 'Get user error',
     GET_PROFILE_SUCCESS: 'Get profile successful',
     GET_PROFILE_ERROR: 'Get profile error',
     UPDATE_PROFILE_SUCCESS: 'Update profile successful',
@@ -15,9 +17,15 @@ export const RESPONSE_MESSAGES = Object.freeze({
     EMAIL_EXIST: 'Email already exist',
   },
   RECIPE: {
+    GET_RECIPE_SUCCESS: 'Get recipe successful',
+    GET_RECIPE_ERROR: 'Get recipe error',
     READY_TO_COOK: 'Ready to cook',
     CAN_NOT_COOK: 'Can not cook this recipe',
     RECOMMEND_FAILED: 'Recommend failed',
     RECOMMEND_SUCCESS: 'Recommend successful',
+  },
+  INGREDIENT: {
+    GET_INGREDIENT_SUCCESS: 'Get ingredient successful',
+    GET_INGREDIENT_ERROR: 'Get ingredient error',
   },
 });
